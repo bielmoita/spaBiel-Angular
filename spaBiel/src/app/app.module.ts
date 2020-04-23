5c97fd2e9e4664a49fc03d6eec4636c884df2017
Gabriel Fernando <oficial.gabriel89@gmail.com>
2020-04-23 12:43:23 -0300

    Criação dos componentes - HOME, CONTATO, NAVBAR e FOOTER

--- spaBiel/src/app/app.module.ts
+++ spaBiel/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContatoComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
